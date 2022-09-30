7af13d87f57f6c8494b6cb8e5550a87a42977457
JunedSetiawan <junedhayko502@gmail.com>
2022-09-30 20:47:35 +0700

    add desk

--- mahasiswa.sql
+++ mahasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Sep 2022 pada 06.14
+-- Waktu pembuatan: 30 Sep 2022 pada 15.46
 -- Versi server: 10.4.24-MariaDB
 -- Versi PHP: 8.0.19
 
@@ -24,42 +24,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Struktur dari tabel `mahasiswa`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(100) NOT NULL,
+CREATE TABLE `mahasiswa` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nrp` int(11) NOT NULL,
-  `prodi` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `prodi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data untuk tabel `mahasiswa`
 --
 
-INSERT INTO `user` (`id`, `nama`, `nrp`, `prodi`) VALUES
-(73, 'Juned Setiawan S', 521419489, 'Teknik Informatika');
+INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `prodi`, `created_at`, `updated_at`) VALUES
+(1, 'Juned Setiawan', 51721875, 'Teknik Informatika', '2022-09-29 23:32:58', '2022-09-29 23:32:58'),
+(2, 'Dani', 41789591, 'Teknik Informatika', '2022-09-29 23:32:58', '2022-09-29 23:32:58'),
+(3, 'Jefa', 33219853, 'Sistem Informasi', '2022-09-29 23:32:58', '2022-09-29 23:32:58'),
+(4, 'Udin', 12948219, 'Teknik Telekomunikasi', '2022-09-29 23:32:58', '2022-09-29 23:32:58'),
+(6, 'Reza', 24142811, 'Ilmu Kimia', NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `user`
+-- Indeks untuk tabel `mahasiswa`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `mahasiswa_nrp_unique` (`nrp`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
 --
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+ALTER TABLE `mahasiswa`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
